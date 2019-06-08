34b5b0eac07dc619f18e2d56eaff02e48a189ea7
schneider <schneider@blinkenlichts.net>
2019-06-08 16:56:16 +0200

    wip: dualcore support

--- Hello_World/main.c
+++ Hello_World/main.c
@@ -21,12 +21,24 @@
 /***** Globals *****/
 static const gpio_cfg_t motor_pin = {PORT_0, PIN_8, GPIO_FUNC_OUT, GPIO_PAD_NONE};
 
+void Core1_Start(void) {
+    //MXC_GCR->gp0 = (uint32_t)(&__isr_vector_core1);
+    MXC_GCR->gp0 = 0x10040000;
+    MXC_GCR->perckcn1 &= ~MXC_F_GCR_PERCKCN1_CPU1;
+}
+
+void Core1_Stop(void) {
+    MXC_GCR->perckcn1 |= MXC_F_GCR_PERCKCN1_CPU1;
+}
+
 int main(void)
 {
     int count = 0;
 
     card10_init();
-    card10_diag();
+    //card10_diag();
+
+    GPIO_Config(&motor_pin);
 
     Paint_DrawImage(Heart, 0, 0, 160, 80);
     LCD_Update();
@@ -36,9 +48,11 @@ int main(void)
     }
 
     int h = 0;
-    while (1) {
-#if 0
+    Core1_Start();
 
+    while (1) {
+
+#if 1
         #define NUM     15
         for(int i=0; i<NUM; i++) {
             if(i < 12) {
@@ -47,12 +61,16 @@ int main(void)
                 leds_set_hsv(i, (h + 360/NUM * i) % 360, 1., 1.);
             }
         }
+        //leds_set_hsv(0, h, 1., 1.);
+        //leds_set_hsv(1, (h + 90) % 360, 1., 1./2);
+        //leds_set_hsv(2, (h + 180) % 360, 1., 1./2);
+        //leds_set_hsv(3, (h + 270) % 360, 1., 1./2);
 
         leds_update();
         TMR_Delay(MXC_TMR0, MSEC(10), 0);
         h++;
 #endif
-#if 1
+#if 0
         pmic_set_led(0, 31);
         pmic_set_led(1, 0);
         pmic_set_led(2, 0);
@@ -79,6 +97,7 @@ int main(void)
         TMR_Delay(MXC_TMR0, MSEC(30), 0);
         GPIO_OutClr(&motor_pin);
         TMR_Delay(MXC_TMR0, MSEC(200), 0);
+
 #endif
 #if 0
         LED_Off(0);
@@ -86,7 +105,7 @@ int main(void)
         LED_On(0);
         TMR_Delay(MXC_TMR0, MSEC(500), 0);
 #endif
-#if 1
+#if 0
         TMR_Delay(MXC_TMR0, MSEC(1000), 0);
         printf("count = %d\n", count++);
 #endif
