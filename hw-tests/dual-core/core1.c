2b73169897bf3607cd70ceab921d3e30b1809351
Rahix <rahix@rahix.de>
2019-06-13 20:09:54 +0200

    fix(dual-core): Don't use a SysTick based delay

--- hw-tests/dual-core/core1.c
+++ hw-tests/dual-core/core1.c
@@ -1,6 +1,6 @@
 #include "board.h"
 #include "gpio.h"
-#include "mxc_delay.h"
+#include "tmr_utils.h"
 
 static const gpio_cfg_t motor_pin = {PORT_0, PIN_8, GPIO_FUNC_OUT, GPIO_PAD_NONE};
 
@@ -11,7 +11,7 @@ int main(void)
 	for (int i = 0; 1; i++) {
 		__asm volatile("wfe");
 		printf("core1: Hello! %d\n", i);
-		mxc_delay(3000000);
+		TMR_Delay(MXC_TMR1, SEC(1), 0);
 		printf("core1: Waking up core0\n");
 		__asm volatile("sev");
 		__asm volatile("wfe");
