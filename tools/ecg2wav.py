6015af16acc4a685cae8e69749bea1deb7e1a769
Rahix <rahix@rahix.de>
2019-12-29 13:00:28 +0100

    chore(ecg2wav): Apply code-style

--- tools/ecg2wav.py
+++ tools/ecg2wav.py
@@ -4,6 +4,7 @@ import wave
 import sys
 import struct
 
+
 def read(file_name):
     signal = numpy.fromfile(file_name, dtype=numpy.int16)
     return signal
@@ -12,19 +13,18 @@ def read(file_name):
 signal = read(sys.argv[1])
 
 
-sampleRate = 128.0   # hertz
-duration = len(signal) / sampleRate   # seconds
+sampleRate = 128.0  # hertz
+duration = len(signal) / sampleRate  # seconds
 
-wavef = wave.open('out.wav','w')
-wavef.setnchannels(1) # mono
-wavef.setsampwidth(2) 
+wavef = wave.open("out.wav", "w")
+wavef.setnchannels(1)  # mono
+wavef.setsampwidth(2)
 wavef.setframerate(sampleRate)
 
 for i in range(int(duration * sampleRate)):
     value = int(signal[i])
-    data = struct.pack('<h', value)
-    wavef.writeframesraw( data )
+    data = struct.pack("<h", value)
+    wavef.writeframesraw(data)
 
 
 wavef.close()
-
