dos1/card10-firmware

commit 4107bd22d34e3f4332c166b6f759fe3f4a4c8c11

hack(ble): Temporarily silence a warning
With the increased warning-level, GCC warns about three unused fields in
`bleSmpCfg`:

    - maxAttemptTimeout: Maximum 'Repeated attempts' timeout in msec
    - attemptDecTimeout: Time msec before attemptExp decreases
    - attemptExp: Exponent to raise attemptTimeout on maxAttempts

These should probably be set to sensible values but to make !360
mergeable, explicitly set them to zero for now.

Signed-off-by: Rahix <rahix@rahix.de>

epicardium/ble/ble_main.c
@@ -122,6 +122,11 @@ static const smpCfg_t bleSmpCfg =
   16,                                     /*! Maximum encryption key length */
   3,                                      /*! Attempts to trigger 'repeated attempts' timeout */
   DM_AUTH_MITM_FLAG,                      /*! Device authentication requirements */
+
+  /* TODO: The following three parameters should probably get proper values */
+  0,                                      /*! Maximum 'Repeated attempts' timeout in msec */
+  0,                                      /*! Time msec before attemptExp decreases */
+  0,                                      /*! Exponent to raise attemptTimeout on maxAttempts */
 };
 
 /* Configuration structure */

commit bdd9e44e2b50289b8062963848fa3dda273fa729

fix(ble): Improve timeout values
Instead of setting these values to 0, use the same settings as used in
lib/sdk/Libraries/BTLE/stack/ble-host/sources/stack/cfg/cfg_stack.c from
the Cordio SDK instead of 0.

In my small test these value sare not use in the normal pairing process,
so I think this is not fixing any problem.

Fixes: 4107bd22d34e ("hack(ble): Temporarily silence a warning")
Signed-off-by: Hauke Mehrtens <hauke@hauke-m.de>

epicardium/ble/ble_main.c
@@ -122,11 +122,9 @@ static const smpCfg_t bleSmpCfg =
   16,                                     /*! Maximum encryption key length */
   3,                                      /*! Attempts to trigger 'repeated attempts' timeout */
   DM_AUTH_MITM_FLAG,                      /*! Device authentication requirements */
-
-  /* TODO: The following three parameters should probably get proper values */
-  0,                                      /*! Maximum 'Repeated attempts' timeout in msec */
-  0,                                      /*! Time msec before attemptExp decreases */
-  0,                                      /*! Exponent to raise attemptTimeout on maxAttempts */
+  64000,                                  /*! Maximum 'Repeated attempts' timeout in msec */
+  64000,                                  /*! Time msec before attemptExp decreases */
+  2,                                      /*! Exponent to raise attemptTimeout on maxAttempts */
 };
 
 /* Configuration structure */